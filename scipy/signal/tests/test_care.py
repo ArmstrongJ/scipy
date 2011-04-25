5535d035cd7d6cf762873fb1a6843462afc2e9ca
ArmstrongJ <approximatrix@gmail.com>
2011-04-25 10:54:11 -0400

    Added a Newton iterative solver to the CARE solver class, including tests.  Minor adjustment to the dare solver code and comments.

--- scipy/signal/tests/test_care.py
+++ scipy/signal/tests/test_care.py
@@ -1,4 +1,4 @@
-from scipy.signal.care import CareSolver
+from scipy.signal.care import CareSolver, continous_riccati_equation, care
 import numpy
 import unittest
 
@@ -38,3 +38,60 @@ class CareTestCase(numpy.testing.TestCase):
             numpy.matrix([[21.7279,14.4853], \
                           [14.4853, 9.6569]]), \
             3)
+            
+    def test_iterative(self):
+        a = numpy.matrix([[0.0,1.0],[0.0,0.0]])
+        b = numpy.matrix([[0.0,],[1.0,]])
+        r = numpy.matrix([[1.0,],])
+        q = numpy.matrix([[1.0,0.0],[0.0,2.0]])
+        
+        cs = CareSolver(a,b,q,r)
+        cs.iterative = True
+        cs.compute_minimizer = True
+        x = cs.solve()
+        
+        numpy.testing.assert_array_almost_equal(abs(x), \
+            numpy.matrix([[2.0,1.0], \
+                          [1.0,2.0]]), \
+            3)
+            
+        # This test example requires careful Schur decomposition
+        a = numpy.matrix([[4.0,3.0],[-9.0/2.0, -7.0/2.0]])
+        b = numpy.matrix([[1.0,],[-1.0,]])
+        q = numpy.matrix([[9.0,6.0],[6.0,4.0]])
+        r = numpy.matrix([[1.0,],])
+        
+        cs = CareSolver(a,b,q,r)
+        cs.iterative = True
+        cs.compute_minimizer = True
+        x = cs.solve()
+        
+        numpy.testing.assert_array_almost_equal(x, \
+            numpy.matrix([[21.7279,14.4853], \
+                          [14.4853, 9.6569]]), \
+            3)
+            
+        # Make sure the convenience function works as expected
+        x = care(a,b,q,r)
+        numpy.testing.assert_array_almost_equal(x, \
+            numpy.matrix([[21.7279,14.4853], \
+                          [14.4853, 9.6569]]), \
+            3)
+
+    def test_continous_riccati_equation(self):
+    
+        a = numpy.matrix([[4.0,3.0],[-9.0/2.0, -7.0/2.0]])
+        b = numpy.matrix([[1.0,],[-1.0,]])
+        q = numpy.matrix([[9.0,6.0],[6.0,4.0]])
+        r = numpy.matrix([[1.0,],])
+        x =  numpy.matrix([[21.7279,14.4853], [14.4853, 9.6569]])
+        
+        ric = continous_riccati_equation(a,b,q,r,x)
+        
+        numpy.testing.assert_array_almost_equal(numpy.zeros((2,2)),ric,3)
+        
+        g = b*numpy.linalg.inv(r)*b.transpose()
+        
+        ric = continous_riccati_equation(a,q,g,x)
+        
+        numpy.testing.assert_array_almost_equal(numpy.zeros((2,2)),ric,3)
