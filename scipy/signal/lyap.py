b4be057890827fae8fdaeccc7891fe3248ceefe5
ArmstrongJ <approximatrix@gmail.com>
2011-04-26 07:53:15 -0400

    Fixed call to LAPACK's Sylvester eqn. solver to be accessed via 'get_lapack_funcs.'  Removed all references to SLICOT.

--- scipy/signal/lyap.py
+++ scipy/signal/lyap.py
@@ -10,15 +10,7 @@ equations
 import numpy
 import scipy.linalg
 
-try:
-    import scipy.linalg.flapack
-except ImportError:
-    pass
-
-try:
-    import slycot
-except ImportError:
-    pass
+from scipy.linalg.lapack import get_lapack_funcs
 
 def sylvester_bartels_stewart(a,b,q):
     """Computes a solution to the Sylvester equation (AX + XB = Q) using the
@@ -38,7 +30,10 @@ def sylvester_bartels_stewart(a,b,q):
     f = u.transpose()*numpy.asmatrix(q)*v
     
     # Call the Sylvester equation solver
-    y,info = scipy.linalg.flapack.dtrsyl(1,r,s,f,1.0,trana=0,tranb=1)
+    trsyl, = get_lapack_funcs(('trsyl',), (r,s,f))
+    if trsyl == None:
+        raise RuntimeError('LAPACK implementation does not contain a Sylvester equation solver (TRSYL)')    
+    y,info = trsyl(1,r,s,f,1.0,trana=0,tranb=1)
     y = numpy.asmatrix(y)
     
     return -u*y*v.transpose()
@@ -50,36 +45,11 @@ def lyap_bartels_stewart(a,q):
         at = numpy.asmatrix(a).getH()
     return sylvester_bartels_stewart(a,at,q)
 
-def lyap_slycot(a,q): 
-    """Solves the continous Lyapunov using the SLICOT library's implementation
-    if available.  The routine attempts to call SB03MD to solve the discrete
-    equation.  If a NameError is thrown, meaning SLICOT is not available,
-    an appropriate RuntimeError is raised.
-    
-    More on SLICOT: http://www.slicot.org/
-    
-    Python Interface (Slycot): https://github.com/avventi/Slycot
-    """
-
-    x = None
-    
-    (m,n) = a.shape
-    if m != n:
-        raise ValueError("input 'a' must be square") 
-    
-    try:
-        x,scale,sep,ferr,w = slycot.sb03md(n, -q, a, numpy.eye(n), 'C', trana='T')
-    except NameError:
-        raise RuntimeError('SLICOT not available')
-    
-    return x
-    
 def lyap(a,q):
     """Solves the continuous Lyapunov equation (AX + XA^H = Q) given the values
     of A and Q.  This function provides a generalized interface to two
-    available solvers. If the Python interface to SLICOT is installed, 
-    the routine will preferentially call the SLICOT solver; otherwise, a Python 
-    implementation of the Bartels-Stewart algorithm is used.
+    available solvers. A Python implementation of the Bartels-Stewart algorithm
+    is used.
     
     Parameters
     ----------
@@ -94,10 +64,7 @@ def lyap(a,q):
         Solution to the continuous Lyapunov equation
     """
     
-    try:
-        return lyap_slycot(a,q)
-    except RuntimeError:
-        return lyap_bartels_stewart(a,q)
+    return lyap_bartels_stewart(a,q)
         
 def sylvester(a,b,q):
     """Solves a general Sylvester equation (AX + XB = Q) via the Bartels-Stewart
