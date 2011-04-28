d87b985d0cbf96cfc6b0f1da5671663e8a4eaf48
ArmstrongJ <approximatrix@gmail.com>
2011-04-28 14:37:55 -0400

    Adding dare and care solvers to __init__.py

--- scipy/signal/__init__.py
+++ scipy/signal/__init__.py
@@ -7,6 +7,8 @@ from info import __doc__
 import sigtools
 from waveforms import *
 from bsplines import *
+from care import *
+from dare import *
 from filter_design import *
 from fir_filter_design import *
 from ltisys import *
