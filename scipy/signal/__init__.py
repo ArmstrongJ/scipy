bf8f7dd39981b9edc6c2dae4feec41d04bd08c8d
ArmstrongJ <approximatrix@gmail.com>
2011-04-28 10:32:36 -0400

    Minor fixes to signal module intialization.

--- scipy/signal/__init__.py
+++ scipy/signal/__init__.py
@@ -8,10 +8,13 @@ import sigtools
 from waveforms import *
 from bsplines import *
 from c2d import *
+from dare import *
 from dltisys import *
+from dlyap import dlyap
 from filter_design import *
 from fir_filter_design import *
 from ltisys import *
+from lyap import lyap, sylvester
 from windows import *
 from signaltools import *
 from spectral import *
